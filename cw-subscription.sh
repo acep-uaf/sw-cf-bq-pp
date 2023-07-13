5ad7966a3632062a761f6bf65ccafb5a9266f12f
jody <thisjody@gmail.com>
2023-07-13 08:48:11 -0700

    roll back workflow and remove the cw files once refactored they can be copied over from ps-bq

--- cw-subscription.sh
+++
@@ -1,4 +0,0 @@
-gcloud pubsub subscriptions create sw-cw-bq-pp-subscription \
-  --topic=sw-cf-bq-pp-dt-rs \
-  --push-endpoint="https://workflowexecutions.googleapis.com/v1/projects/acep-ext-eielson-2021/locations/us-west1/workflows/sw-cw-bq-pp/executions" \
-  --push-auth-service-account=untar-ingest@acep-ext-eielson-2021.iam.gserviceaccount.com

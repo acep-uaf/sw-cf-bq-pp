5a69b4877bc4d6da7a84d46e2f3a8b68247c1760
jody <thisjody@gmail.com>
2023-09-12 07:31:18 -0700

    converted to .env

--- eiedeploy.sh
+++ eiedeploy.sh
@@ -1,10 +1,17 @@
-gcloud functions deploy sw-cw-bq-pp \
-  --gen2 \
-  --runtime=python311 \
-  --region=us-west1 \
-  --source=src \
-  --entry-point=bq_pp \
-  --memory 16384MB \
-  --timeout 540s  \
-  --trigger-topic sw-cf-bq-pp-dt-rs \
-  --set-env-vars PP_TABLE=vtndpp,PUBSUB_TOPIC=sw-cf-bq-pp-gr
+ #!/bin/bash
+
+ # Source the .env file
+ source eiedeploy.env
+
+ # Deploy the function
+ gcloud functions deploy sw-cf-bq-pp \
+   --$GEN2 \
+   --runtime=$RUNTIME \
+   --region=$REGION \
+   --service-account=$SERVICE_ACCOUNT \
+   --source=$SOURCE \
+   --entry-point=$ENTRY_POINT \
+   --memory=$MEMORY \
+   --timeout=$TIMEOUT \
+   --trigger-topic=$TRIGGER_TOPIC \
+   --set-env-vars PP_TABLE=$PP_TABLE,PUBSUB_TOPIC=$PUBSUB_TOPIC
